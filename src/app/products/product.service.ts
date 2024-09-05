735c501cb9e3b7b9eb5e006a367e2c9882144a24
Wilson Dev <hch.wilson@hynkor.com>
2024-09-05 00:48:43 -0500

    Add propiedadres de solo lectura: una forma de evitar sobreescrituras

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = 'dkjfdkjfn' Solo de lectura no editar
   products.push(data);
 }
