6ced765fe67a39d0349df129a1d49d9b0f27d40b
Wilson Dev <hch.wilson@hynkor.com>
2024-09-05 00:43:25 -0500

    Add Extender interfaces: aplicando el concepto de herencia

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,16 @@
-import {} from './products/product.service';
+import {addProduct} from './products/product.service';
 
 addProduct({
   id: '1',
   title: 'p1',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock: 90,
-  size: 'M'
+  size: 'M',
+  category: {
+    id: '12',
+    name: 'c1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
 })
